56d6d4f6ded199b2e6df6a732a028de34504cfe2
Aleksei Saenko <156539224+asaenko01@users.noreply.github.com>
2026-03-01 17:11:28 +0300

    wip

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,4 @@
-import css from '@eslint/css'
 import js from '@eslint/js'
-import json from '@eslint/json'
-import markdown from '@eslint/markdown'
 import perfectionist from 'eslint-plugin-perfectionist'
 import prettier from 'eslint-plugin-prettier/recommended'
 import { defineConfig } from 'eslint/config'
@@ -18,39 +15,11 @@ export default defineConfig([
                 ...globals.node,
             },
         },
-        plugins: { js },
+        plugins: {
+            js,
+        },
     },
     tseslint.configs.recommended,
-    {
-        extends: ['json/recommended'],
-        files: ['**/*.json'],
-        language: 'json/json',
-        plugins: { json },
-    },
-    {
-        extends: ['json/recommended'],
-        files: ['**/*.jsonc'],
-        language: 'json/jsonc',
-        plugins: { json },
-    },
-    {
-        extends: ['json/recommended'],
-        files: ['**/*.json5'],
-        language: 'json/json5',
-        plugins: { json },
-    },
-    {
-        extends: ['markdown/recommended'],
-        files: ['**/*.md'],
-        language: 'markdown/commonmark',
-        plugins: { markdown },
-    },
-    {
-        extends: ['css/recommended'],
-        files: ['**/*.css'],
-        language: 'css/css',
-        plugins: { css },
-    },
     perfectionist.configs['recommended-alphabetical'],
     prettier,
 ])
